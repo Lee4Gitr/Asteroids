a0e5ccb3ac2eab2477cc617467b5bc0cef8cfc72
lee4gitr <lee4gitr@gmail.com>
2024-11-29 23:38:39 -0500

    Added Groups

--- main.py
+++ main.py
@@ -3,18 +3,35 @@
 # throughout this file
 import pygame
 from constants import *
-
+from player import Player
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
-    screen.fill('#16161D')
+        
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        screen.fill('#16161D')
+                
+        dt = clock.tick() / 1000
+        for item in updatable:
+            item.update(dt)
+
+        for item in drawable:
+            item.draw(screen)
+
         pygame.display.flip()
-        
-        
+
 if __name__ == "__main__":
     main()
